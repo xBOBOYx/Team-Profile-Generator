643bd62a9653fbafbff7db2a43fd4b9a65da218d
Anthony Zamora <anthony.zamora@gmail.com>
2021-08-14 21:31:54 -0700

    Engineer pass

---
+++ tests/Engineer.test.js
@@ -0,0 +1,13 @@
+const Engineer = require("../lib/Engineer.js");
+
+test ("Engineer Github input", () => {
+    const github = "xBOBOYx";
+    const employee = new Engineer("Boboy", 123, "boboy@boboy.com", github);
+    expect(employee.github).toBe(github);
+});
+
+test ('"Employee getRole() to return "Engineer"', () => {
+    const role = "Engineer";
+    const employee = new Engineer("Boboy", 123, "boboy@boboy.com");
+    expect(employee.getRole()).toBe(role);
+});
