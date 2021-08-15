c5ca0a20cb1342cb1eb895e03c21ff7cf550e517
Anthony Zamora <anthony.zamora@gmail.com>
2021-08-14 19:20:11 -0700

    Employee test passed

---
+++ tests/Employee.test.js
@@ -0,0 +1,26 @@
+const Employee = require('../lib/Employee.js');
+
+test ('Employee Name', () => {
+    const name = 'John Doe';
+    const employee = new Employee(name);
+    expect(employee.name).toBe(name);
+});
+
+test ('Employee Id', () => {
+    const id = '404';
+    const employee = new Employee("John Doe", id,);
+    expect(employee.id).toBe(id);
+});
+
+test ('Employee getEmail() to return email input', () => {
+    const email = 'john.doe@lost.com';
+    const employee = new Employee("John Doe", 404, email);
+    expect(employee.getEmail()).toBe(email);
+});
+
+test ('Employee getRole() to return "Employee"', () => {
+    const role = "Employee";
+    const employee = new Employee("John Doe", 404, "john.doe@lost.com");
+    expect(employee.getRole()).toBe(role);
+});
+
